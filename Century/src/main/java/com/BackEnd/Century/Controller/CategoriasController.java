619fb2098f09fa9f849291bc197d64a8fa913e3c
Christopher1005 <cmcadenas@soy.sena.edu.co>
2024-11-10 15:53:11 -0500

    actualización categoría

---
+++ Century/src/main/java/com/BackEnd/Century/Controller/CategoriasController.java
@@ -0,0 +1,16 @@
+package com.BackEnd.Century.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1")
+public class CategoriasController {
+
+    @Autowired
+    private 
+    
+}
--- Century/src/main/java/com/BackEnd/Century/Model/Usuario.java
+++ Century/src/main/java/com/BackEnd/Century/Model/Usuario.java
@@ -3,12 +3,16 @@ package com.BackEnd.Century.Model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
+@Getter
+@Setter
 @Table(name = "usuarios")
 public class Usuario {
     @Id
@@ -24,35 +28,7 @@ public class Usuario {
     @Column(name = "password")
     private String password;
 
-    public Long getId() {
-        return id;
-    }
+    @Column(name = "role")
+    private Boolean role;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
